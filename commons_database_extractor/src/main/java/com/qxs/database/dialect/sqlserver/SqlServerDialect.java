debacef073cd4cd402d826b521761e28180a8cf3
459789479@qq.com <q18101077033Q>
2019-04-19 17:48:09 +0800

    增加对SQLServer数据库的支持

--- commons_database_extractor/src/main/java/com/qxs/database/dialect/sqlserver/SqlServerDialect.java
+++ commons_database_extractor/src/main/java/com/qxs/database/dialect/sqlserver/SqlServerDialect.java
@@ -1,8 +1,16 @@
 package com.qxs.database.dialect.sqlserver;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.ibatis.jdbc.SqlRunner;
 import org.springframework.stereotype.Service;
 
 import com.qxs.database.dialect.AbstractDialect;
+import com.qxs.database.model.Database;
 
 /**
  * SQLServer方言
@@ -13,6 +21,10 @@ import com.qxs.database.dialect.AbstractDialect;
  */
 @Service("microsoftsqlserverDialect")
 public class SqlServerDialect extends AbstractDialect {
+	/**
+	 * 获取数据库sql文件名
+	 * **/
+	private static final String SQL_DATABASE_FILE_NAME = "database.sql";
 	/**
 	 * 获取所有的表名sql文件名
 	 * **/
@@ -25,6 +37,10 @@ public class SqlServerDialect extends AbstractDialect {
 	 * 获取列信息sql文件名
 	 * **/
 	private static final String SQL_COLUMNS_FILE_NAME = "columns.sql";
+	/**
+	 * 获取数据库名信息sql
+	 * **/
+	private static final String SQL_DATABASE;
 	/**
 	 * 获取所有的表名sql
 	 * **/
@@ -39,12 +55,25 @@ public class SqlServerDialect extends AbstractDialect {
 	private static final String SQL_COLUMNS;
 	
 	static {
+		SQL_DATABASE = read(SqlServerDialect.class,SQL_DATABASE_FILE_NAME);
 		SQL_ALL_TABLE_NAMES = read(SqlServerDialect.class,SQL_ALL_TABLE_NAMES_FILE_NAME);
 		SQL_TABLE = read(SqlServerDialect.class,SQL_TABLE_FILE_NAME);
 		SQL_COLUMNS = read(SqlServerDialect.class,SQL_COLUMNS_FILE_NAME);
 	}
 	public SqlServerDialect() {
 		super();
+	}
+
+	@Override
+	public List<Database> getDatabases(Connection connection, String databaseVersion) throws SQLException {
+		SqlRunner sqlRunner = new SqlRunner(connection);
+		List<Map<String, Object>> list = sqlRunner.selectAll(SQL_DATABASE);
+		List<Database> databases = new ArrayList<Database>(list.size());
+		for(Map<String, Object> map : list) {
+			String databaseName = map.values().iterator().next().toString();
+			databases.add(new Database(databaseName, databaseName, null));
+		}
+		return databases;
 	}
 
 	@Override
